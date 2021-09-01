7775dd4155f39c5b548e4047230ed71e679bd62f
Carolina Cristino <carolinacristino.studia@mohole.com>
2021-09-01 17:04:00 +0200

    menu funzionante

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const slides =document.querySelectorAll('.slide');
 var button = document.getElementById("button");
 var buttonSlider = document.querySelectorAll('[data-slider="button"]');
 var slider = document.querySelectorAll('.slider');
+
+
 dots.forEach(dot =>{
     dot.addEventListener('click', function(){
         dots.forEach(dt => dt.classList.remove('active'));
@@ -10,10 +12,35 @@ dots.forEach(dot =>{
     });
 });
 
+var menu = document.querySelector(".menu");
+var ham = document.querySelector(".ham");
+var xIcon = document.querySelector(".xIcon");
+var menuIcon = document.querySelector(".menuIcon");
+var xIcon2 = document.querySelector(".xIcon2");
+ham.addEventListener("click", toggleMenu)
 
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    xIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    xIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+xIcon2.addEventListener('click', function(){
+    menu.classList.remove('showMenu');
+})
 
-console.log(buttonSlider);
-console.log(slider);
+var menuLinks = document.querySelectorAll(".menuLink")
+
+menuLinks.forEach(
+  function (menuLink) {
+    menuLink.addEventListener("click", toggleMenu)
+  }
+)
 
 buttonSlider.forEach(
     but =>{ but.addEventListener('click', function(){
